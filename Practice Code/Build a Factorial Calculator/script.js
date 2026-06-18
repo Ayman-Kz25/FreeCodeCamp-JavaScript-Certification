2df971f44159599e236d638e3b371a667d8433a9
Ayman-Kz25 <kamalakz786@gmail.com>
2026-06-18 21:15:50 +0500

    Build a Factorial Calculator

---
+++ Practice Code/Build a Factorial Calculator/script.js
@@ -0,0 +1,17 @@
+const num = 5;
+
+function factorialCalculator(num) {
+  if (num <= 0) {
+    return 0;
+  }
+
+  let result = 1;
+  for (let i = 1; i <= num; i++) {
+    result *= i;
+  }
+  return result;
+}
+
+const factorial = factorialCalculator(num);
+const resultMsg = `Factorial of ${num} is ${factorial}`;
+console.log(resultMsg);
