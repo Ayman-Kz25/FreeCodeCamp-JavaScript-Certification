b15bc3603854b7ab39200470550087d4ea31b8a4
Ayman-Kz25 <kamalakz786@gmail.com>
2026-06-19 13:05:36 +0500

    Build a String Repeating Function

---
+++ Practice Code/Build a String Repeating Function/script.js
@@ -0,0 +1,20 @@
+function repeatStringNumTimes(str, num) {
+  if (num <= 0) {
+    return `""`;
+  }
+
+  let result = "";
+  for (let i = 0; i < num; i++) {
+    result = result + str;
+  }
+
+  return result;
+}
+
+console.log(repeatStringNumTimes("*", 3));
+console.log(repeatStringNumTimes("abc", 3));
+console.log(repeatStringNumTimes("abc", 4));
+console.log(repeatStringNumTimes("abc", 1));
+console.log(repeatStringNumTimes("*", 8));
+console.log(repeatStringNumTimes("abc", -2));
+console.log(repeatStringNumTimes("abc", 0));
