999de09ba5370cfcca0faf31c9cc0c4c275c5bda
Ayman-Kz25 <kamalakz786@gmail.com>
2026-06-30 23:40:56 +0500

    Implement a Value Remover Function

---
+++ Practice Code/Implement a Value Remover Function/script.js
@@ -0,0 +1,17 @@
+function destroyer(arr, ...args) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (args.includes(arr[i])) {
+      continue;
+    }
+    result.push(arr[i]);
+  }
+  return result;
+}
+
+console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
+console.log(destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3));
+console.log(destroyer([3, 5, 1, 2, 2], 2, 3, 5));
+console.log(destroyer([2, 3, 2, 3], 2, 3));
+console.log(destroyer(["tree", "hamburger", 53], "tree", 53));
+console.log(destroyer( ["possum", "trollo", 12, "safari", "hotdog", 92, 65, "grandma", "bugati", "trojan", "yacht"], "yacht", "possum", "trollo", "safari", "hotdog", "grandma", "bugati", "trojan" ));
