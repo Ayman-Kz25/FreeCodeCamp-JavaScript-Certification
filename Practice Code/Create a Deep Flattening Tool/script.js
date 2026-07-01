e5256af05cb521a3afd1e24301eb5324bc4cc657
Ayman-Kz25 <kamalakz786@gmail.com>
2026-07-01 21:57:58 +0500

    Improve Deep Flatten Tool Performance and Efficiency

--- Practice Code/Create a Deep Flattening Tool/script.js
+++ Practice Code/Create a Deep Flattening Tool/script.js
@@ -1,34 +1,22 @@
 function steamrollArray(arr) {
   let result = [];
 
-  for (let i = 0; i < arr.length; i++) {
-    if (!Array.isArray(arr[i])) {
-      result.push(arr[i]);
-    } else {
-      for (let j = 0; j < arr[i].length; j++) {
-        if (!Array.isArray(arr[i][j])) {
-          result.push(arr[i][j]);
-        } else {
-          for (let k = 0; k < arr[i][j].length; k++) {
-            if (!Array.isArray(arr[i][j][k])) {
-              result.push(arr[i][j][k]);
-            } else {
-              for (let m = 0; m < arr[i][j][k].length; m++) {
-                if (!Array.isArray(arr[i][j][k][m])) {
-                  result.push(arr[i][j][k][m]);
-                }
-              }
-            }
-          }
+  function flatten(item){
+      if(!Array.isArray(item)){
+        result.push(item);
+      } else {
+        for(const element of item){
+          flatten(element);
         }
       }
-    }
   }
+
+  flatten(arr);
 
   return result;
 }
 
-// console.log(steamrollArray([[["a"]], [["b"]]]));
-// console.log(steamrollArray([1, [2], [3, [[4]]]]));
-// console.log(steamrollArray([1, [], [3, [[4]]]]));
+console.log(steamrollArray([[["a"]], [["b"]]]));
+console.log(steamrollArray([1, [2], [3, [[4]]]]));
+console.log(steamrollArray([1, [], [3, [[4]]]]));
 console.log(steamrollArray([1, {}, [3, [[4]]]]));
