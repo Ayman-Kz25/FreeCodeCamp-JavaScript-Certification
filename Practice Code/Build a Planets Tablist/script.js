1fe76c90e00858afa8c73867cf0aa4883f4fb7c1
Ayman-Kz25 <kamalakz786@gmail.com>
2026-07-10 00:26:35 +0500

    Build a Planets Tablist

---
+++ Practice Code/Build a Planets Tablist/script.js
@@ -0,0 +1,19 @@
+const tabs = document.querySelectorAll('[role="tab"]');
+const panels = document.querySelectorAll('[role="tabpanel"]');
+
+tabs.forEach(tab => {
+    tab.addEventListener("click", () => {
+        tabs.forEach(t => t.setAttribute("aria-selected", false));
+
+        panels.forEach(panel => panel.hidden = true);
+
+        tab.setAttribute("aria-selected", true);
+
+        const associatedPanel = tab.getAttribute("aria-controls");
+
+        const panel = document.getElementById(associatedPanel);
+
+        panel.hidden = false;
+
+    });
+});
